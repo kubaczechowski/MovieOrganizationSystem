59ef8b3ff0076d78ea11a19f6ebb0aaa90ef03d0
kubaczechowski <jcobczechowski@gmail.com>
2021-01-05 17:54:18 +0100

    added listeners to a movie player

--- src/sample/gui/controller/MoviePlayerController.java
+++ src/sample/gui/controller/MoviePlayerController.java
@@ -1,15 +1,25 @@
 package sample.gui.controller;
 
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.DoubleProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 
 import javafx.fxml.Initializable;
 
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
 import javafx.scene.media.MediaView;
+import javafx.util.Duration;
 
 import java.net.URL;
 import java.nio.file.FileSystems;
@@ -22,21 +32,17 @@ public class MoviePlayerController implements Initializable {
     private String filePath;
 
    @FXML private MediaView mediaView;
+   @FXML private Label label;
+   @FXML private Slider progressBar;
+   @FXML private Slider volumeSlider;
+
    private Media media;
    private MediaPlayer mediaPlayer;
 
-    public MoviePlayerController(String filelink) {
-        this.filePath = filelink;
-    }
 
     public MoviePlayerController() {
     }
 
-    public static MoviePlayerController getInstance() {
-        if(moviePlayerController ==  null)
-            moviePlayerController = new MoviePlayerController();
-        return moviePlayerController;
-    }
 
     public String getFilePath() {
         return filePath;
@@ -65,6 +71,7 @@ public class MoviePlayerController implements Initializable {
             System.out.println("file path is null");
         //create media and media player
         Path path  = FileSystems.getDefault().getPath("src/../Movies/test.mp4");
+        //for now it must be like that in case someone adds a movie i dont have
         media = new Media(path.toUri().toString());
         mediaPlayer = new MediaPlayer(media);
 
@@ -74,6 +81,63 @@ public class MoviePlayerController implements Initializable {
       //play
         mediaPlayer.setAutoPlay(true);
 
+        //other actoion
+        //volume slider
+       setVolumeSlider();
+        //prograss bar
+       setProgressBar();
     }
 
+    private void setVolumeSlider()
+    {
+        volumeSlider.setValue(mediaPlayer.getVolume()*100);
+        volumeSlider.valueProperty().addListener(new InvalidationListener() {
+            @Override
+            public void invalidated(Observable observable) {
+                mediaPlayer.setVolume(volumeSlider.getValue()/100);
+            }
+        });
+    }
+
+    private void setProgressBar()
+    {
+        mediaPlayer.currentTimeProperty().addListener(new ChangeListener<Duration>() {
+            @Override
+            public void changed(ObservableValue<? extends Duration> observable,
+                                javafx.util.Duration oldValue, javafx.util.Duration newValue) {
+                progressBar.setValue(newValue.toSeconds());
+            }
+        });
+        progressBar.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                mediaPlayer.seek(javafx.util.Duration.seconds(progressBar.getValue()));
+            }
+        });
+        progressBar.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                mediaPlayer.seek(javafx.util.Duration.seconds(progressBar.getValue()));
+            }
+        });
+        mediaPlayer.setOnReady(new Runnable() {
+            @Override
+            public void run() {
+                javafx.util.Duration total = media.getDuration();
+                progressBar.setMax(total.toSeconds());
+            }
+        });
+    }
+
+    public void playStopButton(ActionEvent actionEvent) {
+    }
+
+    public void fromTheBeginningButton(ActionEvent actionEvent) {
+    }
+
+    public void toTheEndButton(ActionEvent actionEvent) {
+    }
+
+    public void loopingButton(ActionEvent actionEvent) {
+    }
 }
