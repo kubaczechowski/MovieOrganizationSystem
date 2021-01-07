a6bed4ccbf23ea18b08f0d9e323a7e21a121d5f9
kubaczechowski <jcobczechowski@gmail.com>
2021-01-07 10:23:15 +0100

    web browser update
    - zoom out brows. to 0.6
    - show searching query in textField
    - enable method search
    - added home functionality

--- src/sample/gui/controller/WebBrowserController.java
+++ src/sample/gui/controller/WebBrowserController.java
@@ -1,7 +1,9 @@
 package sample.gui.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
@@ -10,7 +12,11 @@ import java.util.ResourceBundle;
 
 public class WebBrowserController implements Initializable {
     @FXML private WebView webView;
+    @FXML private TextField searchField;
     private  String searchQuery;
+    private String google = "http://www.google.com/search?q=";
+    private String home = "https://www.google.com/webhp";
+    private WebEngine webEngine;
 
     public String getSearchQuery() {
         return searchQuery;
@@ -21,13 +27,28 @@ public class WebBrowserController implements Initializable {
     }
 
     public void openPage(){
-        WebEngine webEngine = webView.getEngine();
-        String google = "http://www.google.com/search?q=";
-        webEngine.load(google+searchQuery);
+        //set textField
+        searchField.setText(google+searchQuery);
+       webEngine.load(google+searchQuery);
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        webView.setZoom(0.6);
+        webEngine = webView.getEngine();
+    }
 
+    public void search(ActionEvent actionEvent) {
+        String usersQuestion = searchField.getText();
+        searchField.setText(google+usersQuestion);
+        webEngine.load(google+usersQuestion);
+    }
+
+    public void home(ActionEvent actionEvent) {
+        searchField.setText(home);
+        webEngine.load(home);
+    }
+
+    public void fullScreen(ActionEvent actionEvent) {
     }
 }
